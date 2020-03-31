9eb68c161b83a8f028f6c73980b4c928feb8aa12
SakthiRamesh <62942950+SakthiRamesh@users.noreply.github.com>
2020-03-31 18:30:40 +0530

    Create naturalnumber.py

---
+++ Python/naturalnumber.py
@@ -0,0 +1,13 @@
+#Write a program to print all natural numbers from 1 to n. - using while loop
+
+
+natural_number = input("Enter the number")
+number = 1
+while (number <= natural_number):
+    print number
+    number += 1
+
+
+number = input("number")
+for i in range(1, number+1):
+    print i
