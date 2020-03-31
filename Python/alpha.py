11a0b62d97300fa8702e2e399b1bf3616caa5699
SakthiRamesh <62942950+SakthiRamesh@users.noreply.github.com>
2020-03-31 18:26:54 +0530

    Create alpha.py

---
+++ Python/alpha.py
@@ -0,0 +1,14 @@
+# Write a program to print all alphabets from a to z.
+
+alphabets = input (" enter the alphabet till you want to print ")
+for alpha in range(65, alphabets + 1):
+        print chr(alpha)
+
+
+
+for alphas in range(65, 91):
+    print chr(alphas)
+
+
+for alp in range(97, 123):
+    print chr(alp)
